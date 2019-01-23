dc88398165931a0496e772a0c101800a4b0125bc
Matej Grgić <grgic.matej@gmail.com>
2019-01-23 12:54:46 +0100

    .

--- Homework/03_homework/index.php
+++ Homework/03_homework/index.php
@@ -17,9 +17,9 @@
 		        	<header><h1>Input</h1></header>
 			        <form method="post">
                 <label for="red"><p class="unostekst">Broj redaka</p></label>
-                <input type="number" class="unos" id="red" min="1" max="10" value="5"/>
+                <input type="number" class="unos" id="red" min="1" value="5"/>
                 <label for="stupac"><p class="unostekst">Broj stupaca</p></label>
-                <input type="number" class="unos" id="stupac" min="1" max="10" value="5"/>
+                <input type="number" class="unos" id="stupac" min="1" value="5"/>
                 <input type="button" id="kreiraj" value="Kreiraj tablicu"/>
               </form>
 		        </section>
