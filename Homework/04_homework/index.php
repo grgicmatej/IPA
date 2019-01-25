e83150f545810a33096de68999009113df27b16e
Matej Grgić <grgic.matej@gmail.com>
2019-01-25 09:23:36 +0100

    .

---
+++ Homework/04_homework/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <?php include_once "head.php"?>
+  </head>
+  <body>
+  <div class="grid-container">
+      <div class="grid-x">
+        <div class="large-12 cell">
+          
+        </div>
+      </div>
+    </div>
+    <?php include_once "skripte.php" ?>
+  </body>
+</html>
