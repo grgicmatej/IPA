7d0ecae3ad0cbe673bb710dae98f4348379284d0
Matej Grgić <grgic.matej@gmail.com>
2019-01-25 09:48:30 +0100

    .

--- Homework/04_homework/index.php
+++ Homework/04_homework/index.php
@@ -6,8 +6,33 @@
   <body>
   <div class="grid-container">
       <div class="grid-x">
+        <div class="large-2 cell"></div>
+        
         <div class="large-12 cell">
-          
+          <table>
+          <caption>Pregled zaposlenika</caption>
+            <tr>
+              <th>Ime zaposlenika</th>
+              <th>Prezime zaposlenika</th>
+              <th>Datum rođenja</th>
+              <th>Spol</th>
+              <th>iznos mjesečnih primanja</th>
+            </tr>
+            <tr>
+              <td>Testni1</td>
+              <td>Prezimeni1</td> 
+              <td>1.1.2000</td>
+              <td>Muški</td>
+              <td>5000.50</td>
+            </tr>
+            <tr>
+              <td>Testni1</td>
+              <td>Prezimeni1</td> 
+              <td>1.1.2000</td>
+              <td>Muški</td>
+              <td>5000.50</td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
